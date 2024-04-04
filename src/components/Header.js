9dd72f9cd896e2f5da83c32fadf89c66a1ecb89b
tamanchichan <tamanchichan@gmail.com>
2024-04-04 17:09:33 -0500

    JavaScript object, 'user', created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 function Header() {
+  const user = {
+    name: "Taman Chi Chan",
+    role: "Fullstack Developer",
+    email: "tamanchichan@gmail.com"
+  }
+  
   return(
     <div className="container">
       <div className="header-user">
-        <h1 className="user-name">Taman Chi Chan</h1>
-        <p className="user-role">Fullstack Developer</p>
-        <p className="user-email"><a href="mailto:tamanchichan@gmail.com">tamanchichan@gmail.com</a></p>
+        <h1 className="user-name">{user.name}</h1>
+        <p className="user-role">{user.role}</p>
+        <p className="user-email"><a href="mailto:tamanchichan@gmail.com">{user.email}</a></p>
       </div>
       <nav className="flex header-menu">
         <ul>
