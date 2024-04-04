707e1ff536e3014497a966f437b180ac1f71a7a3
tamanchichan <tamanchichan@gmail.com>
2024-04-04 18:27:10 -0500

    'div' encapsulated in a section element

--- src/components/Landscape.js
+++ src/components/Landscape.js
@@ -1,16 +1,18 @@
 function Landscape() {
   return(
-    <div className="container">
-      <div className="user-info">
-        <p>
-          Hi, I'm Taman Chi Chan
-        </p>
-        <p>
-          I am an enthusiastic Software Developer eager to turn ideas into
-          creative solutions and always in pursuit of new challenges.
-        </p>
+    <section>
+      <div className="container">
+        <div className="user-info">
+          <p>
+            Hi, I'm Taman Chi Chan
+          </p>
+          <p>
+            I am an enthusiastic Software Developer eager to turn ideas into
+            creative solutions and always in pursuit of new challenges.
+          </p>
+        </div>
       </div>
-    </div>
+    </section>
   )
 };
 
