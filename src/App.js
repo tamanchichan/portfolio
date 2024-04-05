57535bda21932d093e9d1831363458f6f716942d
tamanchichan <tamanchichan@gmail.com>
2024-04-04 19:09:26 -0500

    Skills component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
 import Landscape from "./components/Landscape";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <>
       <header><Header /></header>
-      <main><Landscape /></main>
+      <main>
+        <Landscape />
+        <Skills />
+      </main>
       <footer></footer>
     </>
   );
---
+++ src/components/Skills.js
@@ -0,0 +1,48 @@
+function Skills() {
+  return(
+    <section id="skills">
+        <div class="container">
+          <h2>Technical Skills</h2>
+          <div class="programming-languages">
+            <h3>Programming Languages:</h3>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>C#</li>
+              <li>SQL</li>
+              <li>Java</li>
+              <li>Python</li>
+            </ul>
+          </div>
+          <div class="technologies-frameworks">
+            <h3>Technologies/Frameworks:</h3>
+            <ul>
+              <li>Git</li>
+              <li>Node.js</li>
+              <li>React.js</li>
+              <li>REST APIs</li>
+              <li>Relational Database Design</li>
+              <li>ASP.NET</li>
+            </ul>
+          </div>
+          <div class="developer-tools">
+            <h3>Developer Tools:</h3>
+            <ul>
+              <li>GitHub</li>
+              <li>Visual Studio Code</li>
+              <li>Visual Studio</li>
+              <li>MySQL</li>
+              <li>Postman</li>
+              <li>Azure Data Studio</li>
+              <li>PeopleSoft Test Framework</li>
+              <li>SQL Developer</li>
+              <li>Microsoft Power Automate</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+  )
+};
+
+export default Skills;
