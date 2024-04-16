9ffd319170490db7581b59ea6050aa8b5ce35f59
tamanchichan <tamanchichan@gmail.com>
2024-04-16 14:35:45 -0500

    Projects component created, still in need to add projects to it

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Experience from "./components/Experience";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Landscape from "./components/Landscape";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <main>
         <Landscape />
         <Skills />
+        <Projects />
         <Experience />
         <About />
         <Contact />
---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <section id="projects">
+      <div class="container">
+        <h2>Projects</h2>
+        {/* add projects . . . */}
+      </div>
+  </section>
+  );
+};
+
+export default Projects;
